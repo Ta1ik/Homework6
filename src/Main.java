d6186ea7f3f835aafbf9467f163733d3968152cb
Hw2 <Kucherenko.vv@mail.ru>
2022-10-04 10:29:22 +0800

    HW6 First commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Task1();
+
+    }
+    public static void Task1(){
+        System.out.println("\n" + "Задача 1");
+        /*
+
+         */
+        for (int b = 10; b >= 1; b--) // Уменьшаем значение "b" для следующего шага на 1
+            System.out.print(" " + b); //выводим b в строку
+    }
+}
